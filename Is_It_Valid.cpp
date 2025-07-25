f0bdb26a67a519987dd6f51ba709cfdef7b7bc53
MashrafiGit <mdmasrafi902@gmail.com>
2025-07-25 17:18:12 +0600

    check if the string is empty or not after doing some operation

---
+++ Is_It_Valid.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string str;
+        cin >> str;
+
+        stack<char> s;
+
+        for (char ch : str)
+        {
+            if (!s.empty() && ((s.top() == '0' && ch == '1') || (s.top() == '1' && ch == '0')))
+            {
+                s.pop();
+            }
+            else
+            {
+                s.push(ch);
+            }
+        }
+
+        if (s.empty())
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
